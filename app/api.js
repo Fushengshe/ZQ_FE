24cf9d0ebc79b5576f50f1431474e9def928374e
GTX <guoqingzhe@hotmail.com>
2017-09-30 11:29:24 +0800

    API 迁移

--- app/api.js
+++ app/api.js
@@ -2,7 +2,7 @@
  * Created by GTX on 2017/9/29.
  */
 
-const BASE_URL = 'http://www.thmaoqiu.cn/poetry/public/index.php';
+const BASE_URL = 'http://zqb.ghostio.cn/ZQ_BE/public/index.php';
 export default () => {
   return BASE_URL
 }
